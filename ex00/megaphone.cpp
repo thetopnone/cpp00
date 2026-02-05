794bbe5b138f25e66782042cd1ac8b5bf0c4365f
Alexandros Konstantakis <alexkonstadakis@gmail.com>
2026-02-05 20:45:05 +0200

    this is the cpp00 ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std ::endl;
+		return (0);
+	}
+	std::string input;
+
+	for (int i = 1; i < argc; i++)
+	{
+		input = argv[i];
+		for (int j = 0; j < input[j]; j++)
+			std::cout << (char)toupper(input[j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
